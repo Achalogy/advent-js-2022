a2ddb7738b37322e35f3aaaa27d337cc49ef21e0
Achalogy <achalogy@gmail.com>
2022-12-01 20:11:54 -0500

    Optimización Reto 1

--- reto_1.js
+++ reto_1.js
@@ -1,17 +1,11 @@
 function wrapping(gifts) {
-  let regalos_envueltos = []
-
-  gifts.map(gift => {
-    const envoltorio_vertical = "*".repeat((gift.split("")).length + 2)
-
-    regalos_envueltos.push(
+  return gifts.map(gift => {
+    const envoltorio_vertical = "*".repeat(gift.length + 2)
+    return (
       envoltorio_vertical + "\n*" + gift + "*\n" + envoltorio_vertical
     )
   })
-
-  return regalos_envueltos
 }
-
 
 const gifts = ['book', 'game', 'socks']
 const wrapped = wrapping(gifts)
