ea26f8233c875d9689c7342a722daf1cc1cfde23
Achalogy <62490806+Achalogy@users.noreply.github.com>
2022-12-06 12:42:50 -0500

    Función más optimizada

--- retos/reto-5/index.js
+++ retos/reto-5/index.js
@@ -29,10 +29,23 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
 /* Esta es una versión más compacta que se basa en las mismas cosas */
 
 function getMaxGiftsOPTIMIZED (giftsCities, maxGifts, maxCities) { 
-  return Math.max(...giftsCities
-    .reduce((x, y) => x.concat(x.map(x => [y].concat(x))), [[]])
-    .filter(x => x.length <= maxCities)
-    .map(x => x.reduce((r, s) => r + s, 0))
-    .filter(x => x <= maxGifts)) }
+  return Math.max(
+    ...giftsCities
+      .sort((x, y) => y - x)
+      .reduce((result, _, i) => {
+        if (i) giftsCities.unshift(giftsCities.pop());
+
+        i = giftsCities
+          .slice(0, maxCities)
+          .reduce((acc, curr) => (acc += curr), 0);
+        i <= maxGifts && result.push(i);
+
+        i - giftsCities[maxCities - 1] <= maxGifts &&
+          result.push(i - giftsCities[maxCities - 1]);
+        return result;
+      }, []),
+    0
+  );
+}
 
 module.exports = getMaxGifts
