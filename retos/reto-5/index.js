76da687ee3e30a704f2feb74cd040f7e6e61b481
Achalogy <>
2022-12-06 12:45:41 -0500

    🚀 Función Optimizada

--- retos/reto-5/index.js
+++ retos/reto-5/index.js
@@ -32,18 +32,19 @@ function getMaxGiftsOPTIMIZED (giftsCities, maxGifts, maxCities) {
   return Math.max(
     ...giftsCities
       .sort((x, y) => y - x)
-      .reduce((result, _, i) => {
-        if (i) giftsCities.unshift(giftsCities.pop());
-
-        i = giftsCities
-          .slice(0, maxCities)
-          .reduce((acc, curr) => (acc += curr), 0);
-        i <= maxGifts && result.push(i);
-
-        i - giftsCities[maxCities - 1] <= maxGifts &&
-          result.push(i - giftsCities[maxCities - 1]);
-        return result;
-      }, []),
+      .reduce(
+        (result, _, i) => (
+          i && giftsCities.unshift(giftsCities.pop()),
+          (i = giftsCities
+            .slice(0, maxCities)
+            .reduce((acc, curr) => (acc += curr), 0)),
+          i <= maxGifts && result.push(i),
+          i - giftsCities[maxCities - 1] <= maxGifts &&
+          result.push(i - giftsCities[maxCities - 1]),
+          result
+        ),
+        []
+      ),
     0
   );
 }
