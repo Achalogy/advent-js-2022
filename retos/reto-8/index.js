5e5ddc9a0a02690b05fc7ef513da5fb51d5373df
Achalogy <>
2022-12-08 13:57:59 -0500

    🚀 Función Optimizada

--- retos/reto-8/index.js
+++ retos/reto-8/index.js
@@ -5,4 +5,11 @@ function checkPart(part) {
   }, 0) > 0
 }
 
+function checkPartOptimized(part) {
+  return [...part].some((_, i, x) => {
+    const tr = x.filter((_, k) => i != k);
+    return tr.join("") == tr.reverse().join("");
+  });
+}
+
 module.exports = checkPart
