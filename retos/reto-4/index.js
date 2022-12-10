58386e97661279b34a5eeaa6bc7d4aeb08cda7ad
Achalogy <>
2022-12-10 16:59:46 -0500

    🚀 Mejor Solución Reto 4

--- retos/reto-4/index.js
+++ retos/reto-4/index.js
@@ -1,11 +1,9 @@
 function fitsInOneBox(boxes) {
   return boxes.sort((a, b) => {
-    // Ordena las cajas, sumando todos sus lados
-    return (a.l + a.w + a.h) - (b.l + b.w + b.h)
-  }).every((box, i) => { // Itera en la lista y hace una comparación con todos los resultados, true false true true = false
-    if( i === 0 ) return true; // Ignora la primer caja ya que no hay una caja en index -1
-    const prev = boxes[i - 1] // Caja anterior y más grande si es valida la lista
-    return box.l > prev.l && box.w > prev.w && box.h > prev.h // valida si la anterior caja es más grande
+    return (b.l + b.w + b.h) - (a.l + a.w + a.h)
+  }).slice(1).every((box, i) => {
+    return box.l < boxes[i].l && box.w < boxes[i].w && box.h < boxes[i].h
   })
 }
+
 module.exports = fitsInOneBox
