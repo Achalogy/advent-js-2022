1d67a7641294930a2d0b90557887e55688565cf2
Achalogy <>
2022-12-15 13:33:57 -0500

    🚀 Función Optimizada

--- retos/reto-15/index.js
+++ retos/reto-15/index.js
@@ -1,17 +1,28 @@
 function decorateTree(base) {
+
+  const dict = {
+    "PP": "P",
+    "BB": "B",
+    "RR": "R",
+    "BP": "R",
+    "PB": "R",
+    "BR": "P",
+    "RB": "P",
+    "PR": "B",
+    "RP": "B"
+  }
+
   base = base.split(" ")
   let list = new Array(base.length - 1).fill(base)
   return list.reduce((total, x) =>
     total.concat(
       [new Array(total.at(-1).length - 1).fill("-").map((_, i) => {
-        let slice = total.at(-1).slice(i, i + 2)
-        let res = ['B', 'P', 'R'].filter(el =>
-          !slice.includes(el)
-        )
-        return res.length == 2 ? slice[0] : res
+        return dict[total.at(-1).slice(i, i + 2).join("")]
       }).flat()]
     )
     , [base]).slice(0, base.length).map(x => x.join(" ")).reverse()
 }
+
+decorateTree('R R P R R')
 
 module.exports = decorateTree
