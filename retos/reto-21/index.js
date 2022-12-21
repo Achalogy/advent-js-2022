9b5b3b1d93960df6aaea36a01a6f3012dd32f579
Achalogy <>
2022-12-21 13:50:44 -0500

    🚀 Comprensibilidad Optimizada

--- retos/reto-21/index.js
+++ retos/reto-21/index.js
@@ -1,17 +1,17 @@
 function printTable(gifts) {
-  gifts = gifts.map(x => [x.name, x.quantity])
+  gifts = gifts.map(x => [x.name, '' + x.quantity])
   gifts.unshift(["Gift", "Quantity"])
 
   let [lengthGift, lengthQuantity] = [
     Math.max(...gifts.map(x => x[0].length)),
-    Math.max(...gifts.map(x => `${x[1]}`.length))
+    Math.max(...gifts.map(x => x[1].length))
   ]
 
   gifts.splice(1, 0, ["-".repeat(lengthGift), "-".repeat(lengthQuantity)])
 
   let tables = gifts.map(x => {
     let name = x[0] + " ".repeat(lengthGift - x[0].length)
-    let quantity = x[1] + " ".repeat(lengthQuantity - `${x[1]}`.length)
+    let quantity = x[1] + " ".repeat(lengthQuantity - x[1].length)
     return "| " + name + " | " + quantity + " |" + "\n"
   }).join("")
 
