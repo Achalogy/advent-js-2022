aa1097196ec9cab84d907e78ac93e9a2e0a93c48
Achalogy <>
2022-12-22 13:23:37 -0500

    🚀 Optimización de hk

--- retos/reto-22/index.js
+++ retos/reto-22/index.js
@@ -1,11 +1,7 @@
 function checkStepNumbers(systemNames, stepNumbers) {
-  return systemNames.reduce((curr, x, i) => {
-    let check = stepNumbers[i] <=
-      stepNumbers[
-      i + 1 + systemNames.slice(i + 1).indexOf(systemNames[i])
-      ]
-    return curr + check
-  }, 0) == systemNames.length
+  return systemNames.every((e, i) => stepNumbers[i] <= stepNumbers[
+    i + systemNames.slice(i + 1).indexOf(e) + 1
+  ])
 }
 
 module.exports = checkStepNumbers
