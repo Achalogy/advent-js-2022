efd8a71bdf0ebc3d2b1db682c31b189ff69ba111
Achalogy <>
2022-12-25 17:47:19 -0500

    🐛 Nuevos test Reto 22

--- retos/reto-22/index.js
+++ retos/reto-22/index.js
@@ -1,7 +1,8 @@
 function checkStepNumbers(systemNames, stepNumbers) {
-  return systemNames.every((e, i) => stepNumbers[i] <= stepNumbers[
-    i + systemNames.slice(i + 1).indexOf(e) + 1
-  ])
+  return systemNames.every((e, i) => stepNumbers[i] < stepNumbers[
+      i + systemNames.slice(i + 1).indexOf(e) + 1
+    ] + !Boolean(systemNames.lastIndexOf(e) - i)
+  )
 }
 
 module.exports = checkStepNumbers
