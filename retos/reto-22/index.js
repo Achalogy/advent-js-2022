a058f1e2d3e69624d9786c3ae350b7aeb71754cd
Achalogy <>
2022-12-25 22:14:06 -0500

    Revert "🐛 Quitar Boolean() Innecesario"
    This reverts commit ba04d933e07e27b4c1148b8f38e6638c58a6b75c.

--- retos/reto-22/index.js
+++ retos/reto-22/index.js
@@ -1,7 +1,7 @@
 function checkStepNumbers(systemNames, stepNumbers) {
   return systemNames.every((e, i) => stepNumbers[i] < stepNumbers[
       i + systemNames.slice(i + 1).indexOf(e) + 1
-    ] + !(systemNames.lastIndexOf(e) - i)
+    ] + !Boolean(systemNames.lastIndexOf(e) - i)
   )
 }
 
